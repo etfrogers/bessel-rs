88ff1977e9c03e1d2243465dfc9edb0df3b1d8e0
Ed Rogers <etfrogers@hotmail.com>
2025-03-01 22:58:34 +0000

    Translate zbesj

--- src/amos/mod.rs
+++ src/amos/mod.rs
@@ -1,2 +1,27 @@
+use thiserror::Error;
+
 mod machine;
+mod translator;
 mod zbesh;
+
+#[derive(Error, Debug)]
+pub enum BesselError {
+    #[error("Invalid input: {details}")]
+    InvalidInput { details: String },
+    #[error("Overflow: order TOO LARGE OR CABS(Z) TOO SMALL OR BOTH")]
+    Overflow, //{ too_large: bool },
+    #[error("Loss of too much significance in output")]
+    LossOfSignificance,
+    #[error("Algorithm failed to terminate")]
+    DidNotConverge,
+}
+
+pub enum HankelKind {
+    First,
+    Second,
+}
+
+pub enum Scaling {
+    Unscaled,
+    Scaled,
+}
