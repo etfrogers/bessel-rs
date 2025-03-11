91b60cd50ab4e5fc7b9681e8ed501bb30c7daeda
Ed Rogers <etfrogers@hotmail.com>
2025-03-11 11:21:22 +0000

    Update Fotran test to test arrays

--- src/amos/test.f90
+++ src/amos/test.f90
@@ -3,16 +3,17 @@
 program test
 
    implicit none
-   double precision :: zr, zi, nu, cyr, cyi
+   double precision :: zr, zi, nu!, cyr, cyi
    integer :: kode, n, nz, ierr
-
+   double precision, DIMENSION (1:9) :: cyr, cyi
    ! x = 10.0
    ! print*, x
 
-   zr = 2.1
+   zr = 35.0001
    zi = 0.0
-   nu = 4.0
-   n = 1
+   nu = 340.0
+   n = 9
+   
    kode = 1
    call zbesj(zr, zi, nu, kode, n, cyr, cyi, nz, ierr)
    print*, cyr
