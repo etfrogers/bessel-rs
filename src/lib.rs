1b22681fc92648e9dc248195fe8ea622d771304f
Ed Rogers <etfrogers@hotmail.com>
2025-04-16 18:08:37 +0100

    Remove template func from lib

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,5 @@ pub fn bessel_j<T: Into<Complex64>>(order: f64, z: T) -> Result<Complex64, Besse
     zbesj(z.into(), order, Scaling::Unscaled, 1).map(|v| v.0[0])
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests;
