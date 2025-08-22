38eac663424e45667a2fa0baf2bc6f5d3bead4a5
Ed Rogers <etfrogers@hotmail.com>
2025-08-22 22:09:48 +0100

    Update fortran test code to last saved point

--- src/amos/test.f90
+++ src/amos/test.f90
@@ -4,18 +4,19 @@ program test
 
    implicit none
    double precision :: zr, zi, nu!, cyr, cyi
-   integer :: kode, n, nz, ierr
+   integer :: kode, n, nz, ierr, kind
    double precision, DIMENSION (1:9) :: cyr, cyi
    ! x = 10.0
    ! print*, x
 
-   zr = 35.0001
-   zi = 0.0
-   nu = 340.0
+   nu = 172302836.50840142
+   zr = 1.2494954195932068e-254
+   zi = -981457506.3179193
+   kind = 2
    n = 9
-   
+
    kode = 1
-   call zbesj(zr, zi, nu, kode, n, cyr, cyi, nz, ierr)
+   call zbesi(zr, zi, nu, kode, n, cyr, cyi, nz, ierr)
    print*, cyr
    print*, cyi
    print*, nz
