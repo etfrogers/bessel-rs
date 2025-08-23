c45980ef15c5b3c652fca863d0dc36e0acaf9b75
Ed Rogers <etfrogers@hotmail.com>
2025-08-23 11:48:49 +0100

    Update coverage script to use llvm-cov

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,3 @@
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test "$@"
-grcov . --binary-path ./target/debug/deps/ -s . -t lcov --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/tests.lcov
-grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
-rm cargo-test-*.profraw
+cargo llvm-cov --html "$@"
+cargo llvm-cov report --lcov --output-path lcov.info
+open target/llvm-cov/html/index.html
