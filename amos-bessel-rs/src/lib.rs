1d8121fc15957300b1c65af0044fe5b07732c6cb
Ed Rogers <etfrogers@hotmail.com>
2026-05-17 21:19:30 +0100

    Fix single use statement - makes branches easier later

--- amos-bessel-rs/src/lib.rs
+++ amos-bessel-rs/src/lib.rs
@@ -82,13 +82,13 @@ use crate::reflections::{
     as_integer, integer_sign, reflect_h_element, reflect_i_element, reflect_j_element,
     reflect_y_element,
 };
-pub use crate::types::{BackTo, BesselError};
 pub use amos::{GammaError, HankelKind, Scaling};
 pub use amos::{
     complex_airy, complex_airy_b, complex_bessel_h, complex_bessel_i, complex_bessel_j,
     complex_bessel_k, complex_bessel_y, complex_hankel1, complex_hankel2,
 };
 use num::{Complex, complex::Complex64};
+pub use types::{BackTo, BesselError};
 
 // TODO work with abritrary bit-depth floats
 // TODO bessel derivatives
